41445e724aff6b54bcfb984dc49c02f4379e52e9
Jennifer <jnarvaez07@gmail.com>
2021-11-21 22:09:27 -0600

    Milestone 2, CST150.

---
+++ InventoryTracker/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            //create an inventory item
+            string itemName = itemNameInput.Text;
+            string itemID = idInput.Text;
+            string itemDescription = descriptionInput.Text;
+            decimal itemPrice = decimal.Parse(priceInput.Text);
+            int itemCategory = int.Parse(categoryInput.Text);
+
+
+            InventoryItem item = new InventoryItem(itemName, itemID, itemDescription, itemPrice, itemCategory);
+            MessageBox.Show(item.ShowAllItemDetails());
+
+            
+        }
+    }
+    
+}
+
